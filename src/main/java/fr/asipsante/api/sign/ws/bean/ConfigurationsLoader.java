aaa2d4318aad7b27164b9bda402cf9405fb671cf
cnader <cnader@4cfcd01c-db38-48b1-8200-c3c7a2811bfb>
2020-05-20 15:24:56 +0000

    esignsante-ws

---
+++ src/main/java/fr/asipsante/api/sign/ws/bean/ConfigurationsLoader.java
@@ -0,0 +1,234 @@
+/**
+ * (c) Copyright 1998-2019, ASIP. All rights reserved.
+ */
+package fr.asipsante.api.sign.ws.bean;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Arrays;
+import java.util.Properties;
+
+import org.apache.commons.lang.exception.ExceptionUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import fr.asipsante.api.sign.bean.parameters.CertificateValidationParameters;
+import fr.asipsante.api.sign.bean.parameters.SignatureParameters;
+import fr.asipsante.api.sign.bean.parameters.SignatureValidationParameters;
+import fr.asipsante.api.sign.utils.AsipSignServerException;
+import fr.asipsante.api.sign.ws.util.WsVars;
+
+/**
+ * The Class ConfigurationsLoader.
+ */
+public class ConfigurationsLoader {
+
+    /**
+     * The log.
+     */
+    private static final Logger log = LoggerFactory.getLogger(ConfigurationsLoader.class);
+
+    /**
+     * The Constant CONF_PREFIX.
+     */
+    private static final String CONF_PREFIX = "config.sign.";
+
+    /**
+     * The ws conf path.
+     */
+    private String wsConfPath;
+
+    /**
+     * Enable/disable secret.
+     */
+    private String secretEnabled;
+
+    /**
+     * Instantiates a new configurations loader.
+     *
+     * @param secretEnabled the conf for accepting secrets
+     * @param wsConfPath    the ws conf path
+     */
+    public ConfigurationsLoader(String secretEnabled, String wsConfPath) {
+        super();
+        this.wsConfPath = wsConfPath;
+        this.secretEnabled = secretEnabled;
+    }
+
+    /**
+     * Load.
+     *
+     * @param paramsList the params list
+     */
+    public void load(Parameters paramsList) {
+
+        final Properties props = new Properties();
+        final InputStream targetStream;
+        String[] signIds = new String[0];
+        String[] verifSignIds = new String[0];
+        String[] verifCertIds = new String[0];
+        String[] proofIds = new String[0];
+        try {
+            targetStream = new FileInputStream(wsConfPath);
+            props.load(targetStream);
+            signIds = getProperty(props, "config.sign.ids").trim().split(",");
+            verifSignIds = getProperty(props, "config.verifsign.ids").trim().split(",");
+            verifCertIds = getProperty(props, "config.verifcert.ids").trim().split(",");
+            proofIds = getProperty(props, "config.proof.ids").trim().split(",");
+        } catch (final IOException e) {
+            log.error(WsVars.CONF_PATH_ERROR.getVar(), wsConfPath);
+            log.error(ExceptionUtils.getFullStackTrace(e));
+        }
+
+        for (final String signId : signIds) {
+            final String signPathValue = getProperty(props, CONF_PREFIX + signId + WsVars.PROPS_PATH.getVar());
+            final String signProofId = getProperty(props, CONF_PREFIX + signId + WsVars.PROPS_PROOF.getVar());
+            final SignatureParameters params;
+            try {
+                params = loadConfSign(signPathValue);
+                paramsList.getSignatureConfigurations().put(signId, params);
+                paramsList.getSignatureProofId().put(signId, signProofId);
+
+                if ("enable".equalsIgnoreCase(secretEnabled)) {
+                    final String[] signSecrets = getProperty(props,
+                            CONF_PREFIX + signId + WsVars.PROPS_SECRET.getVar()).trim().split(" ");
+                    paramsList.getSignatureSecrets().put(signId, signSecrets);
+                }
+            } catch (AsipSignServerException | IllegalArgumentException e) {
+                log.warn(WsVars.CONF_ERROR.getVar(), signPathValue);
+                log.warn(ExceptionUtils.getFullStackTrace(e));
+            }
+
+        }
+
+        for (final String verifSignId : verifSignIds) {
+            final String verifPathvalue = getProperty(props,
+                    "config.verifsign." + verifSignId + WsVars.PROPS_PATH.getVar());
+            final SignatureValidationParameters params = loadConfSignValidation(verifPathvalue);
+            paramsList.getSignatureValidationConfigurations().put(verifSignId, params);
+        }
+
+        for (final String verifCertId : verifCertIds) {
+            final String certPathValue = getProperty(props,
+                    "config.verifcert." + verifCertId + WsVars.PROPS_PATH.getVar());
+            final CertificateValidationParameters params = loadConfCertValidation(certPathValue);
+            paramsList.getCertificateValidationConfigurations().put(verifCertId, params);
+        }
+        // Attention, les conf de la preuve sont des conf de signature de la preuve,
+        // les autres paramètres sont récupérés depuis la requête.
+        for (final String proofId : proofIds) {
+            final String proofPathValue = getProperty(props,
+                    "config.proof." + proofId + WsVars.PROPS_PATH.getVar());
+            try {
+                final SignatureParameters params = loadConfSign(proofPathValue);
+                paramsList.getProofSignatureConfigurations().put(proofId, params);
+            } catch (AsipSignServerException | IllegalArgumentException e) {
+                log.warn(WsVars.CONF_ERROR.getVar(), proofPathValue);
+                log.warn(ExceptionUtils.getFullStackTrace(e));
+            }
+        }
+    }
+
+    /**
+     * gets property.
+     *
+     * @param props the properties
+     * @param key   the key
+     * @return the value
+     * @throws IllegalArgumentException missing parameter in property file
+     */
+    private String getProperty(Properties props, String key) {
+        final String value = props.getProperty(key);
+        if (value == null) {
+            log.error(WsVars.CONF_MISSING.getVar(), key, wsConfPath);
+            throw new IllegalArgumentException();
+        }
+        return value;
+    }
+
+    /**
+     * Load conf sign.
+     *
+     * @param confPath the conf path
+     * @return the signature parameters
+     * @throws AsipSignServerException the asip sign server exception
+     */
+    private SignatureParameters loadConfSign(String confPath) throws AsipSignServerException {
+
+        final Properties props = new Properties();
+        try (final FileInputStream fis = new FileInputStream(confPath)) {
+            props.load(fis);
+        } catch (final IOException e) {
+            log.error(WsVars.CONF_PATH_ERROR.getVar(), confPath);
+            log.error(ExceptionUtils.getFullStackTrace(e));
+        }
+
+        final String uniquepassword = System.getProperty("asipsign.ws.unique.password");
+        if (uniquepassword != null) {
+            props.setProperty("pkcs12Password", uniquepassword);
+        }
+
+        return new SignatureParameters(props);
+    }
+
+    /**
+     * Load conf sign validation.
+     *
+     * @param confPath the conf path
+     * @return the signature validation parameters
+     */
+    private SignatureValidationParameters loadConfSignValidation(String confPath) {
+
+        final Properties props = new Properties();
+        final SignatureValidationParameters signValidationParams = new SignatureValidationParameters();
+        try (FileInputStream fis = new FileInputStream(confPath)){
+            props.load(fis);
+            signValidationParams.setDescription(props.getProperty("description", ""));
+            final String rules = props.getProperty("rules");
+            if (rules != null) {
+                signValidationParams.loadRules(Arrays.asList(rules.trim().split(",")));
+            }
+            final String metadata = props.getProperty("metadata");
+            if (metadata != null) {
+                signValidationParams.loadMetadata(Arrays.asList(metadata.trim().split(",")));
+            }
+        } catch (final IOException e) {
+            log.error(WsVars.CONF_PATH_ERROR.getVar(), confPath);
+            log.error(ExceptionUtils.getFullStackTrace(e));
+        }
+        return signValidationParams;
+    }
+
+    /**
+     * Load conf cert validation.
+     *
+     * @param confPath the conf path
+     * @return the certificate validation parameters
+     */
+    private CertificateValidationParameters loadConfCertValidation(String confPath) {
+
+        final Properties props = new Properties();
+        FileInputStream fis = null;
+        final CertificateValidationParameters certValidationParams = new CertificateValidationParameters();
+        try {
+            fis = new FileInputStream(confPath);
+            props.load(fis);
+            certValidationParams.setDescription(props.getProperty("description", ""));
+            final String rules = props.getProperty("rules");
+            if (rules != null) {
+                certValidationParams.loadRules(Arrays.asList(rules.trim().split(",")));
+            }
+            final String metadata = props.getProperty("metadata");
+            if (metadata != null) {
+                certValidationParams.loadMetadata(Arrays.asList(metadata.trim().split(",")));
+            }
+            fis.close();
+        } catch (final IOException e) {
+            log.error(WsVars.CONF_PATH_ERROR.getVar(), confPath);
+            log.error(ExceptionUtils.getFullStackTrace(e));
+        }
+        return certValidationParams;
+    }
+
+}
---
+++ src/test/java/fr/asipsante/api/sign/ws/api/requests/SignatureApiIntegrationTest.java
@@ -0,0 +1,275 @@
+/**
+ * (c) Copyright 1998-2019, ASIP. All rights reserved.
+ */
+package fr.asipsante.api.sign.ws.api.requests;
+
+import static org.junit.Assert.assertNotNull;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.mock.web.MockMultipartFile;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import fr.asipsante.api.sign.config.AsipSignConfigurations;
+import fr.asipsante.api.sign.config.CACRLConfig;
+import fr.asipsante.api.sign.config.ScheduledConfig;
+import fr.asipsante.api.sign.config.WebConfig;
+
+/**
+ * The Class SignatureApiIntegrationTest.
+ */
+@RunWith(SpringRunner.class)
+@ContextConfiguration(classes = { AsipSignConfigurations.class, CACRLConfig.class, ScheduledConfig.class,
+        WebConfig.class })
+@SpringBootTest
+@AutoConfigureMockMvc
+@ComponentScan("fr.asipsante.api.sign.ws.api")
+public class SignatureApiIntegrationTest {
+
+    /** The mock mvc. */
+    @Autowired
+    private MockMvc mockMvc;
+
+    /** The xml. */
+    private MockMultipartFile xml;
+
+    /** The texte. */
+    private MockMultipartFile texte;
+
+    static {
+        final String confPath = Thread.currentThread().getContextClassLoader().getResource("asip-sign-ws.properties")
+                .getPath();
+        if (confPath != null) {
+            System.setProperty("ws.conf", confPath);
+        }
+    }
+
+    /**
+     * Inits the.
+     *
+     * @throws Exception the exception
+     */
+    @Before
+    public void init() throws Exception {
+        xml = new MockMultipartFile("file", Thread.currentThread().getContextClassLoader()
+                .getResourceAsStream("Fichier_TOMWS2_SANS_SIGNATURE.xml"));
+
+        texte = new MockMultipartFile("file",
+                Thread.currentThread().getContextClassLoader().getResourceAsStream("outerjoint.txt"));
+        assertNotNull("Le fichier n'a pas été lu.", xml);
+        assertNotNull("Le fichier n'a pas été lu.", texte);
+    }
+
+    /**
+     * Cas non passant de signature XMLDSIG avec un certificat expiré.
+     *
+     * @throws Exception the exception
+     */
+    @Test
+    public void signatureXMLdsigCertExpire() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.fileUpload("/signatures/xmldsig").file(xml)
+                .param("secret", "lalalalalala").param("idSignConf", "5").accept("application/json"))
+                .andExpect(status().isServiceUnavailable()).andDo(print());
+    }
+
+    /**
+     * Cas non passant de signature XMLDSIG avec preuve avec un certificat expiré.
+     *
+     * @throws Exception the exception
+     */
+    @Test
+    public void signatureXMLdsigWithProofCertExpire() throws Exception {
+        mockMvc.perform(
+                MockMvcRequestBuilders.fileUpload("/signatures/xmldsigwithproof").file(xml).param("secret", "password")
+                        .param("idSignConf", "3").param("idVerifSignConf", "1").param("requestId", "Request-1")
+                        .param("proofTag", "MonTAG").param("applicantId", "RPPS").accept("application/json"))
+                .andExpect(status().isServiceUnavailable()).andDo(print());
+    }
+
+    /**
+     * Cas non passant de signature XMLDSIG avec un certificat révoqué.
+     *
+     * @throws Exception the exception
+     */
+    @Test
+    public void signatureXMLdsigCertRevoque() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.fileUpload("/signatures/xmldsig").file(xml)
+                .param("secret", "poopopioppopop").param("idSignConf", "6").accept("application/json"))
+                .andExpect(status().isServiceUnavailable()).andDo(print());
+    }
+
+    /**
+     * Cas non passant de signature XMLDSIG avec preuve avec un certificat révoqué.
+     *
+     * @throws Exception the exception
+     */
+    @Test
+    public void signatureXMLdsigWithProofCertRevoque() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.fileUpload("/signatures/xmldsigwithproof").file(xml)
+                .param("secret", "lalalalalala").param("idSignConf", "5").param("idVerifSignConf", "1")
+                .param("requestId", "Request-1").param("proofTag", "MonTAG").param("applicantId", "RPPS")
+                .accept("application/json")).andExpect(status().isServiceUnavailable()).andDo(print());
+    }
+
+    /**
+     * Cas non passant de signature XMLDSIG avec preuve avec un certificat avec
+     * mauvais usage.
+     *
+     * @throws Exception the exception
+     */
+    @Test
+    public void signatureXMLdsigWithProofCertBadUsage() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.fileUpload("/signatures/xmldsigwithproof").file(xml)
+                .param("secret", "poopopioppopop").param("idSignConf", "6").param("idVerifSignConf", "1")
+                .param("requestId", "Request-1").param("proofTag", "MonTAG").param("applicantId", "RPPS")
+                .accept("application/json")).andExpect(status().isServiceUnavailable()).andDo(print());
+    }
+
+    /**
+     * Cas passant signature XMLDSIG sans preuve.
+     *
+     * @throws Exception the exception
+     */
+    @Test
+    public void signatureXMLdsigTestNoProof() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.fileUpload("/signatures/xmldsig").file(xml).param("secret", "123456")
+                .param("idSignConf", "1").accept("application/json")).andExpect(status().is2xxSuccessful())
+                .andDo(print());
+    }
+
+    /**
+     * Cas non passant signature XMLDSIG sans preuve, secret incorrect.
+     *
+     * @throws Exception the exception
+     */
+    @Test
+    public void signatureXMLdsigTestNoProofWrongSecret() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.fileUpload("/signatures/xmldsig").file(xml)
+                .param("secret", "wrongSecret").param("idSignConf", "1").accept("application/json"))
+                .andExpect(status().isUnauthorized()).andDo(print());
+    }
+
+    /**
+     * Cas non passant signature XMLDSIG sans preuve, sans secret.
+     *
+     * @throws Exception the exception
+     */
+    @Test
+    public void signatureXMLdsigTestNoProofNoSecret() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.fileUpload("/signatures/xmldsig").file(xml).param("idSignConf", "1").accept("application/json"))
+                .andExpect(status().isUnauthorized()).andDo(print());
+    }
+
+    /**
+     * Cas passant signature XMLDSIG avec preuve.
+     *
+     * @throws Exception the exception
+     */
+    @Test
+    public void signatureXMLdsigTestWithProof() throws Exception {
+        mockMvc.perform(
+                MockMvcRequestBuilders.fileUpload("/signatures/xmldsigwithproof").file(xml).param("secret", "password")
+                        .param("idSignConf", "1").param("idVerifSignConf", "1").param("requestId", "Request-1")
+                        .param("proofTag", "MonTAG").param("applicantId", "RPPS").accept("application/json"))
+                .andExpect(status().is2xxSuccessful()).andDo(print());
+    }
+
+    /**
+     * Cas non passant signature XMLDSIG avec preuve, secret incorrect.
+     *
+     * @throws Exception the exception
+     */
+    @Test
+    public void signatureXMLdsigTestWithProofWrongSecret() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.fileUpload("/signatures/xmldsigwithproof").file(xml)
+                .param("secret", "wrongSecret").param("idSignConf", "1").param("idVerifSignConf", "1")
+                .param("requestId", "Request-1").param("proofTag", "MonTAG").param("applicantId", "RPPS")
+                .accept("application/json")).andExpect(status().isUnauthorized()).andDo(print());
+    }
+
+    /**
+     * Erreur 404.
+     *
+     * @throws Exception the exception
+     */
+    @Test
+    public void signatureXMLdsigTestWrongId() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.fileUpload("/signatures/xmldsig").file(xml).param("secret", "123456")
+                .param("idSignConf", "100").accept("application/json")).andExpect(status().isNotFound()).andDo(print());
+    }
+
+    /**
+     * Erreur 404.
+     *
+     * @throws Exception the exception
+     */
+    @Test
+    public void signatureXMLdsigTestWrongAllIds() throws Exception {
+        mockMvc.perform(
+                MockMvcRequestBuilders.fileUpload("/signatures/xmldsigwithproof").file(xml).param("secret", "123456")
+                        .param("idSignConf", "100").param("idVerifSignConf", "100").param("requestId", "Request-1")
+                        .param("proofTag", "MonTAG").param("applicantId", "RPPS").accept("application/json"))
+                .andExpect(status().isNotFound()).andDo(print());
+    }
+
+    /**
+     * Erreur 501. Cas d'une demande de signature d'un fichier non XML enveloppée
+     *
+     * @throws Exception the exception
+     */
+    @Test
+    public void signatureXMLdsigTestWrongFileFormat() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.fileUpload("/signatures/xmldsigwithproof").file(texte)
+                .param("secret", "password").param("idSignConf", "3").param("idVerifSignConf", "1")
+                .param("requestId", "Request-1").param("proofTag", "MonTAG").param("applicantId", "RPPS")
+                .accept("application/json")).andExpect(status().isNotImplemented()).andDo(print());
+    }
+
+    /**
+     * Cas passant signature XADES sans preuve.
+     *
+     * @throws Exception the exception
+     */
+    @Test
+    public void signatureXadesTest() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.fileUpload("/signatures/xadesbaselineb").file(xml)
+                .param("secret", "123456").param("idSignConf", "1").accept("application/json"))
+                .andExpect(status().is2xxSuccessful()).andDo(print());
+    }
+
+    /**
+     * Cas passant signature XADES avec preuve.
+     *
+     * @throws Exception the exception
+     */
+    @Test
+    public void signatureXadesTestWithProof() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.fileUpload("/signatures/xadesbaselinebwithproof").file(xml)
+                .param("secret", "123456").param("idSignConf", "1").param("idVerifSignConf", "1")
+                .param("requestId", "Request-1").param("proofTag", "MonTAG").param("applicantId", "RPPS")
+                .accept("application/json")).andExpect(status().is2xxSuccessful()).andDo(print());
+    }
+
+    /**
+     * Erreur 404.
+     *
+     * @throws Exception the exception
+     */
+    @Test
+    public void signatureXadesTestWrongId() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.fileUpload("/signatures/xadesbaselineb").file(xml)
+                .param("secret", "disabled").param("idSignConf", "100").accept("application/json"))
+                .andExpect(status().isNotFound()).andDo(print());
+    }
+
+}
