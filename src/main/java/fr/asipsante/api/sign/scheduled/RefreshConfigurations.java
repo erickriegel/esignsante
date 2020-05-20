03159770ab495dca3c98b554874706a65c4b3245
cnader <cnader@4cfcd01c-db38-48b1-8200-c3c7a2811bfb>
2020-05-20 15:24:56 +0000

    esignsante-ws

---
+++ src/main/java/fr/asipsante/api/sign/scheduled/RefreshConfigurations.java
@@ -0,0 +1,51 @@
+/**
+ * (c) Copyright 1998-2019, ASIP. All rights reserved.
+ */
+package fr.asipsante.api.sign.scheduled;
+
+import java.io.File;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import fr.asipsante.api.sign.service.ICACRLService;
+
+/**
+ * The Class RefreshConfigurations.
+ */
+@Component
+@ComponentScan("fr.asipsante.api.sign")
+public class RefreshConfigurations {
+
+    /** The ca path. */
+    @Value("${config.ca.path}")
+    private String caPath;
+
+    /** The crl path. */
+    @Value("${config.crl.path}")
+    private String crlPath;
+
+    /** The cacrl service. */
+    @Autowired
+    private ICACRLService cacrlService;
+
+    /**
+     * Refresh ca.
+     */
+    @Scheduled(cron = "${config.ca.scheduling}")
+    public void refreshCa() {
+        cacrlService.loadCA(new File(caPath));
+    }
+
+    /**
+     * Refresh crl.
+     */
+    @Scheduled(cron = "${config.crl.scheduling}")
+    public void refreshCrl() {
+        cacrlService.loadCRL(new File(crlPath));
+    }
+
+}
