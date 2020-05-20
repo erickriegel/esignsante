18b8d78a35effe9f90da58427546ff2ed5f0c749
cnader <cnader@4cfcd01c-db38-48b1-8200-c3c7a2811bfb>
2020-05-20 15:24:56 +0000

    esignsante-ws

---
+++ src/main/java/fr/asipsante/api/sign/config/OpenAPIDocumentationConfig.java
@@ -0,0 +1,108 @@
+/**
+ * (c) Copyright 1998-2019, ASIP. All rights reserved.
+ */
+package fr.asipsante.api.sign.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import org.springframework.web.util.UriComponentsBuilder;
+
+import com.google.common.base.Predicates;
+
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.paths.Paths;
+import springfox.documentation.spring.web.paths.RelativePathProvider;
+import springfox.documentation.spring.web.plugins.Docket;
+import javax.servlet.ServletContext;
+
+/**
+ * The Class OpenAPIDocumentationConfig.
+ */
+@javax.annotation.Generated(value = "org.openapitools.codegen.languages.SpringCodegen", date = "2019-11-19T15:01:00.313+01:00[Europe/Paris]")
+
+@Configuration
+public class OpenAPIDocumentationConfig {
+
+    /**
+     * Api info.
+     *
+     * @return the api info
+     */
+    ApiInfo apiInfo() {
+        return new ApiInfoBuilder().title("ASIP-Sign").description(
+                "API du composant ASIP-Sign.  Ce composant dit de \"signature\" mutualise et homogénéise la mise en oeuvre des besoins autour de la signature.  Il permet aux partenaires de l'ASIP Santé de signer leurs documents ainsi que de vérifier la validité d'une signature ou d'un certificat.     ")
+                .license("").licenseUrl("http://unlicense.org").termsOfServiceUrl("").version("2.0.0.7")
+                .contact(new Contact("", "", "asip-sign@asipsante.fr")).build();
+    }
+
+    /**
+     * Custom implementation.
+     *
+     * @param servletContext the servlet context
+     * @param basePath       the base path
+     * @return the docket
+     */
+    @Bean
+    public Docket customImplementation(ServletContext servletContext,
+            @Value("${openapi.aSIPSign.base-path:/}") String basePath) {
+        return new Docket(DocumentationType.SWAGGER_2).useDefaultResponseMessages(false).select()
+                .apis(RequestHandlerSelectors.basePackage("fr.asipsante.api.sign.ws.api"))
+                .paths(Predicates.not(PathSelectors.regex("/error.*"))).build()
+                .directModelSubstitute(org.threeten.bp.LocalDate.class, java.sql.Date.class)
+                .directModelSubstitute(org.threeten.bp.OffsetDateTime.class, java.util.Date.class).apiInfo(apiInfo());
+    }
+
+    /**
+     * The Class BasePathAwareRelativePathProvider.
+     */
+    class BasePathAwareRelativePathProvider extends RelativePathProvider {
+
+        /** The base path. */
+        private String basePath;
+
+        /**
+         * Instantiates a new base path aware relative path provider.
+         *
+         * @param servletContext the servlet context
+         * @param basePath       the base path
+         */
+        public BasePathAwareRelativePathProvider(ServletContext servletContext, String basePath) {
+            super(servletContext);
+            this.basePath = basePath;
+        }
+
+        /*
+         * (non-Javadoc)
+         * 
+         * @see
+         * springfox.documentation.spring.web.paths.RelativePathProvider#applicationPath
+         * ()
+         */
+        @Override
+        protected String applicationPath() {
+            return Paths.removeAdjacentForwardSlashes(
+                    UriComponentsBuilder.fromPath(super.applicationPath()).path(basePath).build().toString());
+        }
+
+        /*
+         * (non-Javadoc)
+         * 
+         * @see springfox.documentation.spring.web.paths.AbstractPathProvider#
+         * getOperationPath(java.lang.String)
+         */
+        @Override
+        public String getOperationPath(String operationPath) {
+            final UriComponentsBuilder uriComponentsBuilder = UriComponentsBuilder.fromPath("/");
+            return Paths.removeAdjacentForwardSlashes(
+                    uriComponentsBuilder.path(operationPath.replaceFirst("^" + basePath, "")).build().toString());
+        }
+    }
+
+}
---
+++ src/main/java/fr/asipsante/api/sign/ws/api/delegate/ApiDelegate.java
@@ -0,0 +1,42 @@
+/**
+ * (c) Copyright 1998-2019, ASIP. All rights reserved.
+ */
+package fr.asipsante.api.sign.ws.api.delegate;
+
+import java.util.Optional;
+
+import org.springframework.web.context.request.NativeWebRequest;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+import org.springframework.web.context.request.ServletWebRequest;
+
+/**
+ * The Class ApiDelegate.
+ */
+public class ApiDelegate {
+
+    /**
+     * Gets the request.
+     *
+     * @return the request
+     */
+    public Optional<NativeWebRequest> getRequest() {
+        Optional<NativeWebRequest> request = Optional.empty();
+        final ServletRequestAttributes attrs = (ServletRequestAttributes) RequestContextHolder
+                .currentRequestAttributes();
+
+        if (attrs != null) {
+            request = Optional.ofNullable(new ServletWebRequest(attrs.getRequest()));
+        }
+        return request;
+    }
+
+    /**
+     * Gets the accept header.
+     *
+     * @return the accept header
+     */
+    public Optional<String> getAcceptHeader() {
+        return getRequest().map(r -> r.getHeader("Accept"));
+    }
+}
